82e74d1c5805eaa8393e4369da71620b04f92507
Darlington Otchere <darlington.otchere@marginsgroup.com>
2020-10-15 11:00:25 +0000

    Initial commit

---
+++ src/main/java/com/example/mpayment_mysql/MpaymentMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.mpayment_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpaymentMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpaymentMysqlApplication.class, args);
+	}
+
+}
